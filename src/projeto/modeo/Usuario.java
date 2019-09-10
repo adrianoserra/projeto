e0edb7ac4d35a62c6aec46673ab654e62562a933
gegeo <gegeo@DESKTOP-S7G5QAH>
2019-09-09 22:39:43 -0300

    criado a classe usuario

---
+++ src/projeto/modeo/Usuario.java
@@ -0,0 +1,29 @@
+package projeto.modeo;
+
+public class Usuario {
+	private String idUser;
+	private String login;
+	private String senha;
+	
+	public String getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(String idUser) {
+		this.idUser = idUser;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+
+}
