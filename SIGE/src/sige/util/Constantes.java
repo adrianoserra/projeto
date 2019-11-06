d1f37a78d2716819eeb1cdf2a916ed886e1f28e5
Adriano Serra <adriano_sanfly@hotmail.com>
2019-11-06 15:21:16 -0300

    Alteração das páginas para .xhtml, alterado constantes para .xhtml, exclusão das paginas cadastrarEvento.xhtml sobreUsuario, indexUsuario, contatoUsuario  .html e alterado nos arquivos os links para as paginas agora sendo .xhtml

--- SIGE/src/sige/util/Constantes.java
+++ SIGE/src/sige/util/Constantes.java
@@ -2,8 +2,8 @@ package sige.util;
 
 public class Constantes {
      public static final String cadastrarParticipante = "cadastrarParticipante.xhtml";
-     public static final String contato = "contato.html";
-     public static final String sobre = "sobre.html";
-     public static final String index = "index.html";
-
+     public static final String contato = "contato.xhtml";
+     public static final String sobre = "sobre.xhtml";
+     public static final String index = "index.xhtml";
+     
 }
