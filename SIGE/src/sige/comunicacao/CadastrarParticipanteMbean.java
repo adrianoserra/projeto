d14c7dfb2da019cf46b0a4720ad976e3bcd4628c
gebnfo <54650171+gebnfo@users.noreply.github.com>
2019-11-13 10:05:18 -0300

    Update CadastrarParticipanteMbean.java
    adicionado o metodo alterar participante

--- SIGE/src/sige/comunicacao/CadastrarParticipanteMbean.java
+++ SIGE/src/sige/comunicacao/CadastrarParticipanteMbean.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
@@ -12,14 +13,11 @@ import sige.controle.ParticipanteControle;
 import sige.modeo.Participante;
 import sige.util.Util;
 
-
-
-
 @SessionScoped
 @ManagedBean
 public class CadastrarParticipanteMbean {
 
-	private ParticipanteControle participanteControle = new ParticipanteControle(); 
+	private ParticipanteControle participanteControle = new ParticipanteControle();
 	private Util util = new Util();
 	private Participante participante;
 	private String CPF;
@@ -27,22 +25,21 @@ public class CadastrarParticipanteMbean {
 	private String mensagem;
 	private String confirmarSenha;
 	private Participante participanteAutenticado;
-	
-	  @PostConstruct
-      public void inicializar (){
-		  participante = new Participante();
-		  mensagem = "";
-    	  
-		
+
+	@PostConstruct
+	public void inicializar() {
+		participante = new Participante();
+		mensagem = "";
+
 	}
-	  
-	  public void salvar () {
-		  try {
-			  if (validar() && !verificarSeCPFExiste()) {
-			      participanteControle.salvarParticipante(participante);
-			      mensagem = "Participante salvo com sucesso!";
-			      util.exibirDialog("confirmaÁ„o");
-			  }
+
+	public void salvar() {
+		try {
+			if (validar() && !verificarSeCPFExiste()) {
+				participanteControle.salvarParticipante(participante);
+				mensagem = "Participante salvo com sucesso!";
+				util.exibirDialog("confirma√ß√£o");
+			}
 		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -50,128 +47,140 @@ public class CadastrarParticipanteMbean {
 			util.exibirDialog("erro");
 			return;
 		}
-		  
-	  }
-	  
-	  public void deslogar () {
-		  participanteAutenticado = null;
-		  try {
+
+	}
+
+	public void deslogar() {
+		participanteAutenticado = null;
+		try {
 			FacesContext.getCurrentInstance().getExternalContext().redirect("cadastrarParticipante.xhtml");
 		} catch (IOException e) {
 			e.printStackTrace();
-		} 
-	  }
-	  
-	  public void logar () throws IOException {
-		  if (CPF == null || CPF.equals("")) {
-			  mensagem = "O campo CPF È obrigatorio para o login!";
-			  util.exibirDialog("alerta");
-			  return;
-		  } 
-          if (senha == null || senha.equals("")) {
-    			  mensagem = "O campo senha È obrigatorio para o login!";
-    			  util.exibirDialog("alerta");
-    			  return;
-		  }
-		  try {
-			 participanteAutenticado = participanteControle.autenticarUsuario(CPF, senha);
-			  if (participanteAutenticado != null) {
-				  if (participanteAutenticado.getTipoUsuario() == 0) {
-					  FacesContext.getCurrentInstance().getExternalContext().redirect("inicioADM.xhtml");  
-				  } else {
-					  FacesContext.getCurrentInstance().getExternalContext().redirect("eventos.xhtml");
-				  }
-				  
-			  } else {
-				  mensagem = "a senha ou matrÌcula informadas est„o erradas";
-					util.exibirDialog("alerta");
-			  }
+		}
+	}
+
+	public void logar() throws IOException {
+		if (CPF == null || CPF.equals("")) {
+			mensagem = "O campo CPF √© obrigatorio para o login!";
+			util.exibirDialog("alerta");
+			return;
+		}
+		if (senha == null || senha.equals("")) {
+			mensagem = "O campo senha √© obrigatorio para o login!";
+			util.exibirDialog("alerta");
+			return;
+		}
+		try {
+			participanteAutenticado = participanteControle.autenticarUsuario(CPF, senha);
+			if (participanteAutenticado != null) {
+				if (participanteAutenticado.getTipoUsuario() == 0) {
+					FacesContext.getCurrentInstance().getExternalContext().redirect("inicioADM.xhtml");
+				} else {
+					FacesContext.getCurrentInstance().getExternalContext().redirect("eventos.xhtml");
+				}
+
+			} else {
+				mensagem = "a senha ou matr√≠cula informadas est√£o erradas";
+				util.exibirDialog("alerta");
+			}
 		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			mensagem = "Erro grave, entre em contato com o administrador";
 			util.exibirDialog("erro");
 		}
-	  }
-	  
-	  public boolean validar (){
-		  if (participante.getSenha().equals("")) {
-			  mensagem = "O campo senha È obrigatorio!";
-			  util.exibirDialog("alerta");
-			  return false;
-		  }
-		  if (!confirmarSenha.equals(participante.getSenha())) {
-			  mensagem = "As senhas informadas est„o diferentes!";
-			  util.exibirDialog("alerta");
-			  return false;
-		  }
-		  if (participante.getSetor().equals("")) {
-			  mensagem = "O campo setor È obrigatorio!";
-			  util.exibirDialog("alerta");
-			  return false;
-		  }
-		  if (participante.getNome().equals("")) {
-			  mensagem = "O campo nome È obrigatorio!";
-			  util.exibirDialog("alerta");
-			  return false;
-		  }
-		  if (participante.getSetor().equals("")) {
-			  mensagem = "O campo setor È obrigatorio!";
-			  util.exibirDialog("alerta");
-			  return false;
-		  }
-		  if (participante.getCPF().equals("")) {
-			  mensagem = "O campo CPF È obrigatorio!";
-			  util.exibirDialog("alerta");
-			  return false;
-		  }
-		  if (participante.getTelefone().equals("")) {
-			  mensagem = "O campo telefone È obrigatorio!";
-			  util.exibirDialog("alerta");
-			  return false;
-		  }
-		  if (participante.getEmail().equals("")) {
-			  mensagem = "O campo email È obrigatorio!";
-			  util.exibirDialog("alerta");
-			  return false;
-		  }
-		  return true;
-		  
-	  }
-	  
-	  public boolean verificarSeCPFExiste () {
-		  boolean existe = false;
-			existe = participanteControle.verificarSeCPFExiste(participante.getCPF());
-					if (existe) {
-			  mensagem = "CPF j· cadastrado, informe outro CPF!";
-			  util.exibirDialog("alerta");
-		  }
-		  return existe;
-	  }
-	  
-	  public Boolean mostrarTelaCadastro () {
-		  if (participanteAutenticado == null) {
-			  return true;
-		  } else {
-			  return false;
-		  }
-	  }
-	  
-	  public Boolean mostrarSair () {
-		  if (participanteAutenticado != null) {
-			  return true;
-		  } else {
-			  return false;
-		  }
-	  }
-	  public void paginaCadastro() {
-		  try {
+	}
+
+	public boolean validar() {
+		if (participante.getSenha().equals("")) {
+			mensagem = "O campo senha √© obrigatorio!";
+			util.exibirDialog("alerta");
+			return false;
+		}
+		if (!confirmarSenha.equals(participante.getSenha())) {
+			mensagem = "As senhas informadas est√£o diferentes!";
+			util.exibirDialog("alerta");
+			return false;
+		}
+		if (participante.getSetor().equals("")) {
+			mensagem = "O campo setor √© obrigatorio!";
+			util.exibirDialog("alerta");
+			return false;
+		}
+		if (participante.getNome().equals("")) {
+			mensagem = "O campo nome √© obrigatorio!";
+			util.exibirDialog("alerta");
+			return false;
+		}
+		if (participante.getSetor().equals("")) {
+			mensagem = "O campo setor √© obrigatorio!";
+			util.exibirDialog("alerta");
+			return false;
+		}
+		if (participante.getCPF().equals("")) {
+			mensagem = "O campo CPF √© obrigatorio!";
+			util.exibirDialog("alerta");
+			return false;
+		}
+		if (participante.getTelefone().equals("")) {
+			mensagem = "O campo telefone √© obrigatorio!";
+			util.exibirDialog("alerta");
+			return false;
+		}
+		if (participante.getEmail().equals("")) {
+			mensagem = "O campo email √© obrigatorio!";
+			util.exibirDialog("alerta");
+			return false;
+		}
+		return true;
+
+	}
+
+	public boolean verificarSeCPFExiste() {
+		boolean existe = false;
+		existe = participanteControle.verificarSeCPFExiste(participante.getCPF());
+		if (existe) {
+			mensagem = "CPF j√° cadastrado, informe outro CPF!";
+			util.exibirDialog("alerta");
+		}
+		return existe;
+	}
+
+	public Boolean mostrarTelaCadastro() {
+		if (participanteAutenticado == null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public Boolean mostrarSair() {
+		if (participanteAutenticado != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void paginaCadastro() {
+		try {
 			FacesContext.getCurrentInstance().getExternalContext().redirect("cadastrarParticipante.xhtml");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	  }
+	}
+	
+	public void alterarParticipante () {
+		FacesContext context = FacesContext.getCurrentInstance();
+		try {
+			participanteControle.alterarParticipante(participanteAutenticado);
+			context.addMessage(null, new FacesMessage("Dados alterados com sucesso!", " ") );
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public Participante getParticipante() {
 		return participante;
 	}
@@ -179,7 +188,7 @@ public class CadastrarParticipanteMbean {
 	public void setParticipante(Participante participante) {
 		this.participante = participante;
 	}
-	
+
 	public String getSenha() {
 		return senha;
 	}
@@ -219,6 +228,5 @@ public class CadastrarParticipanteMbean {
 	public void setParticipanteAutenticado(Participante participanteAutenticado) {
 		this.participanteAutenticado = participanteAutenticado;
 	}
-	
 
 }
